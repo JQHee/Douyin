5b91c24b243eb706fef958e2d2fbe0e749360c6a
HJQ <v-huangjiaqian@midland.local>
2019-01-09 17:37:43 +0800

    创建基础功能框架

--- Douyin/Douyin/AppDelegate.swift
+++ Douyin/Douyin/AppDelegate.swift
@@ -14,7 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        self.window?.rootViewController = UIViewController()
+        self.window?.makeKeyAndVisible()
         return true
     }
 
---
+++ Douyin/Douyin/Classes/Sections/test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  Douyin
+//
+//  Created by midland on 2019/1/9.
+//
+
+import Foundation
