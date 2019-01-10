e001461d3fec3dd2236c2dffa72aef84c28f57aa
HJQ <v-huangjiaqian@midland.local>
2019-01-10 14:39:32 +0800

    增加navigation

--- Douyin/Douyin/Classes/Sections/Main/SwipeBackBaseViewController.swift
+++ Douyin/Douyin/Classes/Sections/Main/SwipeBackBaseViewController.swift
@@ -17,11 +17,40 @@ class SwipeBackBaseViewController: UINavigationController {
         self.delegate = self
     }
     
+    private func setupNavBar() {
+        let navBar = UINavigationBar.appearance()
+        /// 把这个半透明关闭，不然会影响布局
+        navBar.isTranslucent = false
+        navBar.barTintColor = UIColor.orange
+        navBar.barStyle = .black
+        navBar.tintColor = UIColor.white
+        navBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18)]
+        navBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationBar.shadowImage = UIImage()
+    }
+    
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         if animated {
             self.interactivePopGestureRecognizer?.isEnabled = false
         }
+        if viewControllers.count > 0 {
+            // 进入新页面隐藏tabbar
+            viewController.hidesBottomBarWhenPushed = true
+            viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "icon_titlebar_whiteback"), style: .plain, target: self, action: #selector(backAction))
+        }
+        
+        let backItem = UIBarButtonItem.init(title: "", style: UIBarButtonItem.Style.plain, target: nil, action: nil)
+        viewController.navigationItem.backBarButtonItem = backItem
+        
         super.pushViewController(viewController, animated: animated)
+        // 获取tabBar的frame, 如果没有直接返回
+        guard var frame = self.tabBarController?.tabBar.frame else {
+            return
+        }
+        // 设置frame的y值, y = 屏幕高度 - tabBar高度
+        frame.origin.y = UIScreen.main.bounds.size.height - frame.size.height
+        // 修改tabBar的frame
+        self.tabBarController?.tabBar.frame = frame
     }
     
 }
@@ -32,7 +61,8 @@ extension SwipeBackBaseViewController: UIGestureRecognizerDelegate {
         if let touchButton = touch.view as? UIButton {
             touchButton.isHighlighted = true
         }
-        return true
+        let point = touch.location(in: self.view)
+        return point.x >= 0 && point.x < ceil(UIScreen.main.bounds.size.width) / 3.0
     }
     
     
@@ -47,7 +77,17 @@ extension SwipeBackBaseViewController: UIGestureRecognizerDelegate {
             touchButton.isHighlighted = false
         }
         
-        return true
+        // 3. 不在转场过程中
+        guard let isTransitioning = self.value(forKey: "_isTransitioning") as? NSNumber else { return false }
+        return !isTransitioning.boolValue
+    }
+    
+    /// 解决 scrollView 的滑动手势 和 右滑返回手势 冲突问题
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard otherGestureRecognizer is UIPanGestureRecognizer else { return false }
+        guard let otherGestureView = otherGestureRecognizer.view as? UIScrollView else { return false }
+        guard otherGestureView.bounces && otherGestureView.alwaysBounceHorizontal else { return false }
+        return otherGestureView.contentOffset.x <= 0
     }
     
 }
@@ -55,5 +95,11 @@ extension SwipeBackBaseViewController: UIGestureRecognizerDelegate {
 extension SwipeBackBaseViewController: UINavigationControllerDelegate {
     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
         interactivePopGestureRecognizer?.isEnabled = true
+    }
+}
+
+extension SwipeBackBaseViewController {
+    @objc func backAction() {
+        self.navigationController?.popViewController(animated: true)
     }
 }
--- Douyin/Douyin/Classes/Sections/UserHomePage/UserHomePageViewController.swift
+++ Douyin/Douyin/Classes/Sections/UserHomePage/UserHomePageViewController.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 
+
 class UserHomePageViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navBarBgAlpha = 0
         self.view.backgroundColor = UIColor.white
     }
 
